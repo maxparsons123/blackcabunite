2d362b2671539467c9a02594b665e1d19f99de04
maxparsons123 <115324986+maxparsons123@users.noreply.github.com>
2025-10-03 19:01:05 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,9 @@
-// service-worker.js
 self.addEventListener('notificationclick', (event) => {
   event.notification.close();
   event.waitUntil(
     clients.matchAll({ type: 'window' }).then((clientList) => {
       if (clientList.length > 0) {
-        const client = clientList[0];
-        client.focus();
-        // Optional: send message to app
-        if (event.notification.data) {
-          client.postMessage({
-            type: 'fcm-notification-click',
-            data: event.notification.data
-          });
-        }
+        clientList[0].focus();
       } else {
         clients.openWindow('./');
       }
