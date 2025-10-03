957ef03ebf80bb33a70a11dd3b23bd6f8c3e53f0
maxparsons123 <115324986+maxparsons123@users.noreply.github.com>
2025-10-03 01:04:38 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,21 @@
-const CACHE_NAME = 'bcu-cache-v1';
+const CACHE_NAME = 'bcu-v1';
 const urlsToCache = [
-  '/blackcabunite/',
-  '/blackcabunite/index.html',
-  '/blackcabunite/manifest.json',
-  '/blackcabunite/icon-192.png',
-  '/blackcabunite/icon-512.png'
+  '/',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/mqtt/dist/mqtt.min.js'
 ];
 
-// Install: cache files
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Activate: clean old caches
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(
-      keys.filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-    ))
-  );
-});
-
-// Fetch: cache-first strategy
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(fetchResponse => {
-        if(fetchResponse.ok) {
-          caches.open(CACHE_NAME).then(cache => cache.put(event.request, fetchResponse.clone()));
-        }
-        return fetchResponse;
-      });
-    }).catch(() => {
-      if(event.request.mode === 'navigate') {
-        return caches.match('/blackcabunite/index.html');
-      }
-    })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
