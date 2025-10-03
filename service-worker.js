99e43bddb98babc50de9fa3e206633e2063c5a28
maxparsons123 <115324986+maxparsons123@users.noreply.github.com>
2025-10-03 16:50:30 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,21 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker installed');
+// service-worker.js
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then((clientList) => {
+      if (clientList.length > 0) {
+        const client = clientList[0];
+        client.focus();
+        // Optional: send message to app
+        if (event.notification.data) {
+          client.postMessage({
+            type: 'fcm-notification-click',
+            data: event.notification.data
+          });
+        }
+      } else {
+        clients.openWindow('./');
+      }
+    })
+  );
 });
-self.addEventListener('fetch', (e) => {});
