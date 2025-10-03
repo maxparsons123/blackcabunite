c6f621fc0756dfcdf3158013c489cac414c5d7b6
maxparsons123 <115324986+maxparsons123@users.noreply.github.com>
2025-10-03 01:39:56 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,4 @@
-const CACHE_NAME = 'bcu-v1';
-const urlsToCache = [
-  '/',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
-  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-  'https://unpkg.com/mqtt/dist/mqtt.min.js'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+self.addEventListener('install', (e) => {
+  console.log('Service Worker installed');
 });
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
-});
+self.addEventListener('fetch', (e) => {});
